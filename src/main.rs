5f72dbbfd6d8399eaaa00705c5c0eab517b8eb1d
Mike Craig <mike@mkscrg.net>
2023-04-27 11:09:34 -0700

    Cargo.toml and todo list

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    println!("todo");
+
+    // todo:
+    // - collect cli args
+    // - setup readline + stub background thread + interceptor
+    // - run readline
+    // - implement background thread
 }
